4ab64ca0005164b669d27c76b90b7ad2087ea448
Michael <m.stebbins1120@gmail.com>
2022-03-24 23:54:39 -0500

    Created WebInspectionTool.py

---
+++ WebInspectionTool.py
@@ -0,0 +1,9 @@
+"""
+Website Inspection Tool: Used to examine webpages in the development of new
+web scraping tools for data.
+"""
+import BeautifulSoup as bs
+
+class WebInspector:
+    def __init__(self):
+        pass
