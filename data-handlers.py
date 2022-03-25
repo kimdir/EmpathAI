6d02d669424efc44efe178c3c51137cb211fdff6
Michael <m.stebbins1120@gmail.com>
2022-03-24 23:40:33 -0500

    Create data-handlers.py

---
+++ data-handlers.py
@@ -0,0 +1,9 @@
+"""Defines the handler class for all data import managers."""
+
+class DataHandler:
+    def __init__(self):
+        import twitter-scrape as ts
+        TwitterHandler = ts.TwitterHandler()
+
+if __name__ == ("__main__"):
+    DataHandler = DataHandler()
